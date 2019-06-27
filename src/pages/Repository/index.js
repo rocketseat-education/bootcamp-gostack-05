7198d6b4703f9898011df2906129de3e52479319
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-27 15:07:29 -0300

    12 - Definindo PropTypes

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 // import { Container } from './styles';
 
 export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
   state = {
     repository: {},
     issues: [],
