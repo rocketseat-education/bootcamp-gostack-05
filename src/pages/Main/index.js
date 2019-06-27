bd76cf66bc3021330514b09e569acdb2721f49be
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-27 11:47:08 -0300

    6 - Estilizando página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Hello World</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
