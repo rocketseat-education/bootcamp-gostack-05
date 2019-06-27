87fd8bfeb40796c546a02e6c66bb27f82d9e8f3e
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-27 11:19:15 -0300

    4 - Styled Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Title } from './styles';
 
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Title error={false}>
+      Main
+      <small>menor</small>
+    </Title>
+  );
 }
