de7b782a1f73f1cf323c746c06704bd664a55048
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-27 14:24:35 -0300

    9 - Utilizando LocalStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -12,6 +12,24 @@ export default class Main extends Component {
     loading: false,
   };
 
+  // Carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Salvar os dados do localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
